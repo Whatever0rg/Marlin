5043067b9c7ea286bbbadc89efecbe9793e2d51e
Whatever0rg <whateverdotorg18@gmail.com>
2024-03-06 18:16:07 +0100

    compilable ver.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1566,7 +1566,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -44, -7, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -36.5, -40, -3.29 }
 
 // Enable and set to use a specific tool for probing. Disable to allow any tool.
 #define PROBING_TOOL 0
